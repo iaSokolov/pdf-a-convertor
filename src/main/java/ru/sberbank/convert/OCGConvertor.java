6a153fce046830241ee867a3f054274cf2502898
Sokolov Ilya <iasokolov@gmail.com>
2023-03-13 12:42:51 +0300

    Подключение mokito и покрытие тестами

--- src/main/java/ru/sberbank/convert/OCGConvertor.java
+++ src/main/java/ru/sberbank/convert/OCGConvertor.java
@@ -2,14 +2,12 @@ package ru.sberbank.convert;
 
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfName;
-import ru.sberbank.decoder.StreamDecoder;
 
 public class OCGConvertor implements IConvertItem {
     @Override
     public ConversionStatus changeDocument(PdfDocument document) {
         ConversionStatus result;
         try {
-            new StreamDecoder().decode(document);
             document.getCatalog().remove(PdfName.OCProperties);
 
             result = ConversionStatus.SuccessStatus();
--- src/test/java/ru/sberbank/TestConverter.java
+++ src/test/java/ru/sberbank/TestConverter.java
@@ -1,9 +1,11 @@
 package ru.sberbank;
 
 import org.junit.jupiter.api.*;
-import ru.sberbank.convert.ConversionStatus;
-import ru.sberbank.convert.ConversionStatusCode;
-import ru.sberbank.convert.Converter;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+import ru.sberbank.check.CheckResult;
+import ru.sberbank.check.CheckSourceFileExist;
+import ru.sberbank.convert.*;
 import ru.sberbank.params.ConvertParams;
 
 import java.io.File;
@@ -11,9 +13,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class TestConverter {
     private static final String RESULT_DIR = "tmp/";
@@ -91,5 +97,85 @@ public class TestConverter {
         Assertions.assertFalse(hasError);
 
         Assertions.assertTrue(new File(target).exists());
+    }
+
+    @Test
+    @DisplayName("Test error decode conversion")
+    public void errorDecodeConversionTest() {
+        String source = SOURCE_DIR + "pdfA-1.pdf";
+        String target = RESULT_DIR + "pdfA-1-error-decode-result.pdf";
+
+        try (MockedConstruction<DecoderConvertor> mockConvertor = Mockito.mockConstruction(DecoderConvertor.class, (mock, context) -> {
+            when(mock.changeDocument(any())).thenReturn(ConversionStatus.ErrorStatus("Decode error text"));
+        })) {
+
+            Assertions.assertFalse(new File(target).exists());
+
+            ConvertParams convertParams = new ConvertParams(new String[]{source, target});
+            Converter converter = new Converter(convertParams);
+
+            List<ConversionStatus> conversionStatuses = converter.makeDocument();
+            List<ConversionStatus> errorList = conversionStatuses
+                    .stream()
+                    .filter(it -> it.getCode() == ConversionStatusCode.ERROR)
+                    .collect(Collectors.toList());
+
+            Assertions.assertNotNull(errorList);
+
+            boolean decodeError = errorList
+                    .stream()
+                    .anyMatch(it -> it.getMessage().equals("Decode error text"));
+            Assertions.assertTrue(decodeError);
+        }
+    }
+
+    @Test
+    @DisplayName("Test error OCG conversion")
+    public void errorOCGConversionTest() {
+        String source = SOURCE_DIR + "pdfA-1.pdf";
+        String target = RESULT_DIR + "pdfA-1-error-OCG-result.pdf";
+
+        try (MockedConstruction<OCGConvertor> mockConvertor = Mockito.mockConstruction(OCGConvertor.class, (mock, context) -> {
+            when(mock.changeDocument(any())).thenReturn(ConversionStatus.ErrorStatus("OCG conversion error text"));
+        })) {
+            Assertions.assertFalse(new File(target).exists());
+
+            ConvertParams convertParams = new ConvertParams(new String[]{source, target});
+            Converter converter = new Converter(convertParams);
+
+            List<ConversionStatus> conversionStatuses = converter.makeDocument();
+            List<ConversionStatus> errorList = conversionStatuses
+                    .stream()
+                    .filter(it -> it.getCode() == ConversionStatusCode.ERROR)
+                    .collect(Collectors.toList());
+
+            Assertions.assertNotNull(errorList);
+
+            boolean OCGConversionError = errorList
+                    .stream()
+                    .anyMatch(it -> it.getMessage().equals("OCG conversion error text"));
+            Assertions.assertTrue(OCGConversionError);
+        }
+    }
+
+    @Test
+    @DisplayName("Test error check params")
+    public void errorerrorCheckParamsTest() {
+        String source = SOURCE_DIR + "pdfA-1.pdf";
+        String target = RESULT_DIR + "pdfA-1-error-check-params-result.pdf";
+
+        try (MockedConstruction<CheckSourceFileExist> mockCheck = Mockito.mockConstruction(CheckSourceFileExist.class, (mock, context) -> {
+            List<CheckResult> listResultError = new ArrayList<>();
+            listResultError.add(CheckResult.Error("error line 1"));
+            listResultError.add(CheckResult.Error("error line 2"));
+            when(mock.getResult()).thenReturn(listResultError);
+        })) {
+            Assertions.assertFalse(new File(target).exists());
+
+            ConvertParams convertParams = new ConvertParams(new String[]{source, target});
+
+            RuntimeException error = assertThrows(RuntimeException.class, () -> new Converter(convertParams));
+            Assertions.assertEquals("error line 1\nerror line 2", error.getMessage());
+        }
     }
 }
